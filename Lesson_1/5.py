5abc20cd9198dbb269d9f1ceae5a7d14ad8f0953
zaytsev <zaytsev@slotlogic.eu>
2019-05-16 18:52:36 +0300

    Решил почти все задания, кроме 9. Добавлю решение позже

--- Lesson_1/5.py
+++ Lesson_1/5.py
@@ -1,2 +1,15 @@
 #5.	Пользователь вводит две буквы. Определить, на каких местах
 # алфавита они стоят, и сколько между ними находится букв.
+
+start_postition_alphabet = ord('a')
+symbol_1 = ord(input(f'Введите первую букву:\n')) - start_postition_alphabet + 1
+symbol_2 = ord(input(f'Введите вторую букву:\n')) - start_postition_alphabet + 1
+if symbol_2 > symbol_1:
+    result = symbol_2 - symbol_1 - 1
+elif symbol_1 > symbol_2:
+    result = symbol_1 - symbol_2 - 1
+else:
+    result = 0
+print(f'Позиция первой буквы в алфавите: {symbol_1}\n'
+      f'Позиция второй буквы в алфавите: {symbol_2}\n'
+      f'Между ними букв: {result}\n')
--- Lesson_1/8.py
+++ Lesson_1/8.py
@@ -3,3 +3,12 @@
 #Год является високосным в двух случаях: либо он кратен 4,
 # но при этом не кратен 100, либо кратен 400.
 
+user_year = int(input('Введите год для проверки:\n'))
+result = 'високосный'
+if user_year % 400 == 0:
+    pass
+elif user_year % 100 != 0 and user_year % 4 == 0:
+    pass
+else:
+    result = 'не високосный'
+print(f'год {user_year} - {result}')
