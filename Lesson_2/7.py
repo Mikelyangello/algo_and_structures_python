11d79b6248d3193a169baa00980cda4c9ea62cec
zaytsev <zaytsev@slotlogic.eu>
2019-05-21 15:57:50 +0300

    Первый коммит. Осталось сделать 2 задания (8 и 9)

--- Lesson_2/7.py
+++ Lesson_2/7.py
@@ -3,3 +3,18 @@
 натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
  где n - любое натуральное число.
 """
+
+n = int(input('Введите любое натуральное число для проверки равенства: 1+2+...+n = n(n+1)/2: '))
+
+
+def check_expression(n, left=0, counter=1):
+    while counter <= n:
+        left += counter
+        counter += 1
+        return check_expression(n, left, counter)
+    if left == n * (n + 1) / 2:
+        return print('Равенство существует')
+    return print('Равенство не существует')
+
+
+check_expression(n)
--- Lesson_2/9.py
+++ Lesson_2/9.py
@@ -2,3 +2,12 @@
 9. Среди натуральных чисел, которые были введены, найти
 наибольшее по сумме цифр. Вывести на экран это число и сумму его цифр.
 """
+
+
+numbers = input('Введите натуральные числа для проверки: ').split(' ')
+
+def best_of_number(number, counter, summ):
+    if type(number) is list:
+        print('OK')
+
+print(type(numbers))
