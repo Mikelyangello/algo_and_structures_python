86c522bf4187ab9c4b5fbbff24b1bde3cf51659a
Mikelyangello <hostagik@gmail.com>
2019-06-02 01:09:00 +0300

    Закончил работу над ДЗ

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -5,20 +5,59 @@
 """
 
 
+import timeit
+
 """
 Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
 """
 
-number = 10
-
 
 def summ_of_elements(n, summ=0):
     if n == 0:
-        return print(f'сумма элементов ряда чисел до элемента №{number} равна: {summ}')
+        return summ
     summ += 1 / (2 ** (n - 1)) - 2 / (2 ** (n - 1)) * ((n - 1) % 2)
     n -= 1
     return summ_of_elements(n, summ)
 
 
+def summ_of_elements_by_cycle(n):
+    summ = 0
+    while n > 0:
+        summ += 1 / (2 ** (n - 1)) - 2 / (2 ** (n - 1)) * ((n - 1) % 2)
+        n -= 1
+    return summ
 
+
+recursion = timeit.timeit('summ_of_elements(500)',
+                          setup="from __main__ import summ_of_elements",
+                          number=1000)
+cycle = timeit.timeit('summ_of_elements_by_cycle(500)',
+                      setup="from __main__ import summ_of_elements_by_cycle",
+                      number=1000)
+print(f'(при вход.данных = 500)\n'
+      f'{recursion:.6f} - рекурсия\n'
+      f'{cycle:.6f} - цикл\n'
+      f'отношение = {(float(cycle)/float(recursion)*100):.0f}%')
+
+'''
+
+Сложность алгоритма линейная O(n)
+Рекурсия проигрывает циклу в среднем в диапазоне 10-20%.
+
+(при вход.данных = 5)
+0.005160 - рекурсия
+0.004814 - цикл
+отношение = 93%
+
+(при вход.данных = 50)
+0.055709 - рекурсия
+0.045625 - цикл
+отношение = 82%
+
+(при вход.данных = 500)
+1.063535 - рекурсия
+0.886004 - цикл
+отношение = 83%
+
+'''
 
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,76 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+
+
+import timeit
+
+
+def wrap_eratosfen(end, n=None):
+    if n is None:
+        n = end
+
+    def eratosfen(m):
+        erat_arr = list(range(m * 2))
+        erat_arr[1] = 0
+        for i in erat_arr:
+            if i > 1:
+                for j in range(i + i, len(erat_arr), i):
+                    erat_arr[j] = 0
+        return erat_arr
+
+    result_arr = eratosfen(n)
+    count_of_just_numbers = len(result_arr) - result_arr.count(0)
+    if count_of_just_numbers < end:
+        n += 1
+        return wrap_eratosfen(end, n)
+    else:
+        counter = 1
+        for i in result_arr:
+            if i != 0:
+                if counter == end:
+                    return i
+                counter += 1
+
+
+def wrap_cycle(end, n):
+    def cycle(n):
+        lst = []
+        for i in range(2, n + 1):
+            for j in range(2, i):
+                if i % j == 0:
+                    break
+            else:
+                lst.append(i)
+        return lst
+    while len(cycle(n)) < end:
+        n += 1
+    return cycle(n).pop()
+
+
+print(timeit.timeit("wrap_eratosfen(10)", setup="from __main__ import wrap_eratosfen", number=100))
+print(timeit.timeit("wrap_cycle(10, 2)", setup="from __main__ import wrap_cycle", number=100))
+
+
+'''
+Решето работает быстрее в любых случаях, как при маленьких, так и при больших числах
+Связано это с тем, что решето использует умный алгоритм и не перебирает все возможные комбинации цифр.
+Замеры (всегда в количестве 100 запусков):
+
+end = 4
+0.00037033401895314455
+0.001708939002128318
+Решето быстрее на 78%
+
+end = 40
+0.1285964669950772
+1.5947898809972685
+Решето быстрее на 92%
+
+end = 100
+1.4070781960035674
+31.90297833798104
+Решето быстрее на 96%
+
+Предполагаю, что сложность линеарифметическая - O(N log N)
+'''
