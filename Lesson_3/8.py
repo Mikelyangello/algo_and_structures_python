fe4b48f83b83a489558ed50885bbdd52024aa38a
zaytsev <zaytsev@slotlogic.eu>
2024-06-11 17:17:27 +0200

    8

--- Lesson_3/8.py
+++ Lesson_3/8.py
@@ -4,3 +4,25 @@
 записывать ее в последнюю ячейку строки.
 В конце следует вывести полученную матрицу.
 """
+
+matrix = []
+maximum = 0
+
+for i in range(5):
+    b = []
+    row_sum = 0
+    for j in range(4):
+        new_el = int((input(f"Input element in matrix your position now: row-{i+1} col-{j+1}: ")))
+        row_sum += new_el
+        b.append(new_el)
+
+    b.append(row_sum)
+    maximum = maximum if maximum > row_sum else row_sum
+    matrix.append(b)
+
+maximum = len(str(maximum)) + int(len(str(maximum)) / 2)
+
+for i in matrix:
+    for j in i:
+        print(f"{j:<{maximum}}", end='')
+    print()
